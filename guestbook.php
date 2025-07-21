fe4b2cfdde4ddf19d29234f77e995d846b794be1
stefanie-weiss-neuland <stefanie.weiss@neuland-bfi.de>
2025-07-21 21:28:37 +0200

    Formular Test, ToDo: No Safe

---
+++ guestbook.php
@@ -0,0 +1,32 @@
+<form method="post" action="index.php">
+    Name: <input type="text" name="name"><br>
+    Nachricht: <textarea name="nachricht"></textarea><br>
+    <input type="submit" value="Abschicken">
+</form>
+
+<?php
+$guestbookFile = "guestbook.txt";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $nachricht = htmlspecialchars($_POST["nachricht"]);
+
+    if (empty($name)){
+        echo "Bitte geben Sie einen Namen ein!";
+    }
+    if (empty($nachricht)){
+        echo "Bitte geben Sie eine Nachricht ein!";
+    }
+    $entry = "$name: $nachricht\n";
+    file_put_contents("guestbook.txt", $entry, FILE_APPEND | LOCK_EX);
+}
+?>
+
+<h2>Einträge:</h2>
+<pre>
+    <?php
+    if (file_exists($guestbookFile)) {
+    echo htmlspecialchars(file_get_contents($guestbookFile));
+    }
+    ?>
+</pre>
